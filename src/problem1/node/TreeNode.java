b2e2001c4c3fbe1bc6ab9eea0d7a5cafa4e5d939
AshishAgarwal639 <ashish.agarwal_cs18@gla.ac.in>
2020-03-27 17:48:31 -0700

    created data,left and right fields and constructor of TreeNode

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,12 @@
 package problem1.node;
 
 public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int key) {
+        data = key;
+        left = right = null;
+    }
 }
