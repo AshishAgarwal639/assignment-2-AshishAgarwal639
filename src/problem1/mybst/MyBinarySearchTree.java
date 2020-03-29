ec99a7aa23b8ff18ba7af40a91918254dbe5dfe8
AshishAgarwal639 <ashish.agarwal_cs18@gla.ac.in>
2020-03-28 17:54:03 -0700

    created constructor and methods like add, addService, preOrder and postOrder

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -14,17 +14,41 @@ public class MyBinarySearchTree {
     public TreeNode root;
 
     public MyBinarySearchTree() {
-        root = null;
+        this.root = null;
         preOrderString = new StringBuilder();
     }
 
+    public static void preOrder(TreeNode root) {
+        if (root == null)
+            return;
+        preOrderString.append(root.getData() + " ");
+        preOrder(root.getLeft());
+        preOrder(root.getRight());
+    }
+
     public void add(final int value) {
-        root = addService(root, value);
+        this.root = addService(this.root, value);
     }
 
     public TreeNode addService(final TreeNode treeRoot, final int addingValue) {
         if (treeRoot == null)
             return new TreeNode(addingValue);
-        if (addingValue < treeRoot.da)
+        if (addingValue < treeRoot.getData())
+            treeRoot.setLeft(addService(treeRoot.getLeft(), addingValue));
+        else if (addingValue == treeRoot.getData())
+            return treeRoot;
+        else
+            treeRoot.setRight(addService(treeRoot.getRight(), addingValue));
+        return treeRoot;
     }
+
+    public void postOrder(TreeNode root) {
+        if (root == null)
+            return;
+        postOrder(root.getLeft());
+        postOrder(root.getRight());
+        System.out.print(root.getData() + " ");
+    }
+
+
 }
