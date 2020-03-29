a95ec007c36e47f587a4f51d9cf833f334bce136
AshishAgarwal639 <ashish.agarwal_cs18@gla.ac.in>
2020-03-28 17:25:19 -0700

    created data,left and right fields and constructor of TreeNode and getter and setter methods

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -6,5 +6,25 @@
  */
 package problem1.mybst;
 // to implement BinarySearchTree
+
+import problem1.node.TreeNode;
 public class MyBinarySearchTree {
+    public static StringBuilder preOrderString;
+    static int levels = 0;
+    public TreeNode root;
+
+    public MyBinarySearchTree() {
+        root = null;
+        preOrderString = new StringBuilder();
+    }
+
+    public void add(final int value) {
+        root = addService(root, value);
+    }
+
+    public TreeNode addService(final TreeNode treeRoot, final int addingValue) {
+        if (treeRoot == null)
+            return new TreeNode(addingValue);
+        if (addingValue < treeRoot.da)
+    }
 }
