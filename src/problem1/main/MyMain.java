6e3bfdb2703bf64f8b724279b9daa9acd690b585
AshishAgarwal639 <ashish.agarwal_cs18@gla.ac.in>
2020-03-28 18:22:06 -0700

    created main method and object of MyBinarySearchTree and take 7 inputs.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,26 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+        int i = 7;
+        while (i > 0) {
+            tree.add(scanner.nextInt());
+            i--;
+        }
+        /*tree looks like
+                4
+               / \
+              2   6
+             / \  / \
+            1   3 5  7
+         */
+        // For only left children 4,2,1,5 should be printed.
+    }
 }
